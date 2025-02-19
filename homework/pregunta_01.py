7009b52d05c12e09751b61ec256bb1279e80131b
Juan Jose Pineda Sanchez <jpinedasa@unal.edu.co>
2025-02-19 14:11:59 -0500

    commit
    commit

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,18 +1,34 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta.
-"""
-
-# pylint: disable=import-outside-toplevel
-
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')  
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 
 def pregunta_01():
-    """
-    Siga las instrucciones del video https://youtu.be/qVdwpxG_JpE para
-    generar el archivo `files/plots/news.png`.
 
-    Un ejemplo de la grafica final esta ubicado en la raíz de
-    este repo.
+    output_dir = "files/plots"
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, "news.png")
 
-    El gráfico debe salvarse al archivo `files/plots/news.png`.
+    data = pd.read_csv("files/input/news.csv", names=["Year", "Television", "Newspaper", "Internet", "Radio"], header=0)
 
-    """
+
+    sns.set_theme(style="darkgrid")
+
+    plt.figure(figsize=(8, 6))
+ 
+    plt.plot(data["Year"], data["Television"], label="Television", color="black", marker="o")
+    plt.plot(data["Year"], data["Newspaper"], label="Newspaper", color="gray", marker="o")
+    plt.plot(data["Year"], data["Internet"], label="Internet", color="blue", marker="o", linewidth=2)
+    plt.plot(data["Year"], data["Radio"], label="Radio", color="lightgray", marker="o")
+
+
+    plt.title("How people get their news")
+    plt.xlabel("Year")
+    plt.ylabel("Percentage")
+    plt.legend()
+
+    plt.savefig(output_path)
+
+pregunta_01()
